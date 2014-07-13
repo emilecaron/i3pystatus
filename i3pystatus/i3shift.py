b43ab76c82267882506f6e62f120cc35f21a4283
emilecaron <emile.caron@outlook.com>
2014-07-13 15:19:45 +0200

    i3shift module updated

--- i3pystatus/i3shift.py
+++ i3pystatus/i3shift.py
@@ -1,17 +1,21 @@
 # -*- coding:utf-8 -*-
 
-from i3pystatus import Module
-from pythonscripts.i3shiftworkspaces import shift_workspaces
+from i3pystatus import IntervalModule
+from pythonscripts.i3shiftworkspaces import shift_workspaces, leftshift_possible
 
-class i3shift(Module):
+class i3shift(IntervalModule):
     """
     Simple shortcut to i3shiftworkspaces module.
     """
 
-    output= {
-        "full_text": "<<<<",
-    }
+    def run(self):
+        self.output= {
+            "full_text": "<<<<",
+            "color": "#ff0000" if leftshift_possible() else "#ffffff",
+        }
+
 
     def on_leftclick(self):
         shift_workspaces(9,False)
+        self.run()
 
